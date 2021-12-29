8724d8c93b7040d3cf3fe5976c30673bd4c96358
duogwas <duong2001n@gmail.com>
2021-12-29 17:53:10 +0700

    tinh lai suat

---
+++ 29.12.2021/TinhLaiSuatNganHang/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TinhLaiSuatNganHang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ten;
+            int tienGui, kyHan;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập tên khách hàng: ");
+            ten = Console.ReadLine();
+            Console.Write("Số tiền muốn gửi: ");
+            tienGui = int.Parse(Console.ReadLine());
+            Console.Write("Nhập kỳ hạn: ");
+            kyHan = int.Parse(Console.ReadLine());
+            Console.ReadLine();
+        }
+    }
+}
