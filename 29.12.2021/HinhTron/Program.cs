f4b35dd5b1ae95fc34bbb00532a15ecf85dc40e4
duogwas <duong2001n@gmail.com>
2021-12-29 17:49:18 +0700

    hinh tron

---
+++ 29.12.2021/HinhTron/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HinhTron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, dientich, chuvi;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập bán kính: ");
+            r = double.Parse(Console.ReadLine());
+            dientich = Math.Pow(r, 2) * Math.PI;
+            chuvi = 2 * Math.PI * r;
+            Console.WriteLine("\nDiện tích hình tròn bán kính {0} là: {1}", r, dientich);
+            Console.WriteLine("\nChu vi hình tròn bán kính {0} là: {1}", r, chuvi);
+            Console.ReadLine();
+        }
+    }
+}
